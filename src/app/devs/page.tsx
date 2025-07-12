7fcdd5a5972a3d793a81bfd116a99380574eff3c
Favourfisayo <Favourfisayo5@gmail.com>
2025-07-12 20:10:11 +0100

    Re-deployment

--- src/app/devs/page.tsx
+++ src/app/devs/page.tsx
@@ -1,23 +1,30 @@
-import sql from "../lib/db"
-import DevCard from "../components/DevCard"
-import { Dev } from "@/app/data/definitions"
+// import sql from "../lib/db"
+// import DevCard from "../components/DevCard"
+// import { Dev } from "@/app/data/definitions"
 
-const devs: Dev[] = await sql<Dev[]> `
-  SELECT name, email, project_title, github_link, site_link, description, tags
-  FROM developers
-  ORDER BY name ASC
-`
+// const devs: Dev[] = await sql<Dev[]> `
+//   SELECT name, email, project_title, github_link, site_link, description, tags
+//   FROM developers
+//   ORDER BY name ASC
+// `
 
+// const page = () => {
+//   return (
+//     <>
+//       {devs.map((dev, index) => 
+//         <DevCard
+//           key={index}
+//           dev={dev}
+//         />
+//       )}
+//     </>
+//   )
+// }
+
+// export default page
 const page = () => {
   return (
-    <>
-      {devs.map((dev, index) => 
-        <DevCard
-          key={index}
-          dev={dev}
-        />
-      )}
-    </>
+    <div>page</div>
   )
 }
 
